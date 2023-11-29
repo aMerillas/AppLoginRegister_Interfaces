8bba2fc19275b443d0719ebf24ff61bd11a2b71d
merillas <adrianmpgg@gmail.com>
2023-11-29 13:28:28 +0100

    Comit entrega2.4 -Splash -Login -Register -Main con reciclerView de arboles -nopeople (sin usar) -SwipeArboles
    ---------------
    
    -PullRequest a nacho

--- app/src/main/java/com/example/login/SwipeArboles.java
+++ app/src/main/java/com/example/login/SwipeArboles.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
@@ -15,10 +17,9 @@ public class SwipeArboles extends AppCompatActivity {
 
     private SwipeRefreshLayout swipe;
     private WebView miWeb;
+    private boolean bloquearRefresh = false;
     List<Item> items = DataHolder.getItems();
-    String mensaje, url ;
-
-
+    String mensaje, url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,6 @@ public class SwipeArboles extends AppCompatActivity {
         swipe = findViewById(R.id.refresco);
         swipe.setOnRefreshListener(mOnRefreshListener);
 
-
         miWeb = findViewById(R.id.web);
         registerForContextMenu(miWeb);
         WebSettings webSettings = miWeb.getSettings();
@@ -37,25 +37,38 @@ public class SwipeArboles extends AppCompatActivity {
         miWeb.loadUrl("https://static.vecteezy.com/system/resources/previews/016/721/112/non_2x/swipe-down-line-color-background-icon-vector.jpg");
     }
 
-    public void getArbol(){
-
+    public void getArbol() {
         Random random = new Random();
         int randomIndex = random.nextInt(items.size());
         Item randomTree = items.get(randomIndex);
-        mensaje =randomTree.getName();
-        url =randomTree.getImg();
-
+        mensaje = randomTree.getName();
+        url = randomTree.getImg();
     }
-    protected SwipeRefreshLayout.OnRefreshListener
-            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+
+    protected SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
         @Override
         public void onRefresh() {
-            getArbol();
-            Toast toast = Toast.makeText(SwipeArboles.this, mensaje, (int) 0.5);
-            toast.show();
-            miWeb.reload();
-            miWeb.loadUrl(url);
-            swipe.setRefreshing(false);
+            if (!bloquearRefresh) {
+                bloquearRefresh = true;
+
+                getArbol();
+                Toast toast = Toast.makeText(SwipeArboles.this, mensaje, Toast.LENGTH_SHORT);
+                toast.show();
+
+                miWeb.reload();
+                miWeb.loadUrl(url);
+
+                new CountDownTimer(Toast.LENGTH_SHORT + 2000, 100) {
+                    public void onTick(long millisUntilFinished) {
+
+                    }
+
+                    public void onFinish() {
+                        swipe.setRefreshing(false);
+                        bloquearRefresh = false;
+                    }
+                }.start();
+            }
         }
     };
 }
